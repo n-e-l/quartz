8f980c006d2191c7779b235af3e2fa8feb047041
Lauda Carly <lauda@nel.re>
2026-01-28 22:27:30 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "svelte devShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in with pkgs; {
+        devShells.default = mkShell rec {
+          buildInputs = [
+            nodejs_22
+			nodePackages.npm
+			git
+          ];
+        };
+      });
+}
